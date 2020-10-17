90fe14fffebc3293355121eefa816ea93d0d1714
RafaLinaresMolina <rafa.linares.molina@gmail.com>
2020-10-17 21:31:23 +0200

    added client router

--- routers/clientRouter.js
+++ routers/clientRouter.js
@@ -1,7 +1,21 @@
-const router = require('express').Router();
-const {createAppointment, cancelAppointment} = require('../controllers/clientController');
+const router = require("express").Router();
+const {
+  createAppointment,
+  cancelAppointment,
+  deactivateAcount,
+  watchHistoryOfAppointments,
+  watchHistoryOfAppointmentsBetweenDates,
+} = require("../controllers/clientController");
+const auth = require("../middleware/auth");
 
-router.post("/appointment", createAppointment);
-router.delete("/appointment", cancelAppointment);
+router.post("/appointment", auth.loggedRequired, createAppointment);
+router.delete("/appointment", auth.loggedRequired, cancelAppointment);
+router.delete("/account", auth.loggedRequired, deactivateAcount);
+router.get("/appointments", auth.loggedRequired, watchHistoryOfAppointments);
+router.get(
+  "/appointmentsbetweenDates",
+  auth.loggedRequired,
+  watchHistoryOfAppointmentsBetweenDates
+);
 
 module.exports = router;
