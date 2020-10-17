7bca9607899fa122a76eae662b91477849a10ae9
RafaLinaresMolina <rafa.linares.molina@gmail.com>
2020-10-17 21:28:46 +0200

    fixed auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
-const ClientModel = require("../models/client");
-const UserModel = require("../models/client");
+const UserModel = require("../models/user");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   try {
     process.log.debug(" -> authController.register");
     process.log.data(req.body);
-    const Client = mongoose.model("Clients", ClientModel.schema, ClientModel.collection);
+    const Client = mongoose.model(
+      "Clients",
+      UserModel.schema,
+      'Users'
+    );
     const clientDoc = new Client(req.body);
     clientDoc.password = await bcrypt.hash(clientDoc.password, 9);
     await clientDoc.save();
@@ -24,7 +27,9 @@ const register = async (req, res) => {
 
 const isValidPassword = async (password, hashPasword) => {
   try {
-    return await bcrypt.compare(password, hashPasword);
+    const isOk = await bcrypt.compare(password, hashPasword);
+    process.log.debug(`Password Match ? ${isOk}`)
+    return isOk;
   } catch (err) {
     throw err;
   }
@@ -41,11 +46,13 @@ const login = async (req, res) => {
     process.log.debug(" -> authController.login");
     process.log.data(req.body);
 
-    const user = await UserModel.findOne({ email: req.body.email });
+    const user = await UserModel.findOne({ email: req.body.email, status: 1 });
     if (!user) {
+      process.log.warning(` <- authController.login: Wrong credentials!`);
       return res.status(401).message({ message: "Wrong credentials!" });
     }
-    await isValidPassword(req.body.password, user.password);
+
+    await isValidPassword(req.body.password, user.toJSON().password);
     const token = generateToken({ _id: user._id, rolId: user.rolId });
     user.token = token;
     await user.save();
@@ -55,7 +62,7 @@ const login = async (req, res) => {
     process.log.error(` x- authController.login ERROR: ${err.message}`);
     res
       .status(500)
-      .send({ message: "Error on authController.login", trace: err });
+      .send({ message: "Error on authController.login", trace: err.message });
   }
 };
 
@@ -63,20 +70,21 @@ const logout = async (req, res) => {
   try {
     process.log.debug(" -> authController.logout");
 
-    if(!req.headers.authorization){
-      process.log.warning(`Token not present in headers`);
-      return res.status(401).send({ message: 'Token not present in headers' })
+    if (!req.headers.authorization) {
+      process.log.warning(` <- authController.logout: Token not present in headers`);
+      return res.status(401).send({ message: "Token not present in headers" });
     }
 
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(" ")[1];
 
     const user = await UserModel.findOne({ token });
     if (!user) {
+      process.log.warning(` <- authController.logout: User already not logged.`);
       return res.status(401).message({ message: "User already not logged." });
     }
     user.token = null;
     await user.save();
-    res.send({ message: 'User logged out successfuly' });
+    res.send({ message: "User logged out successfuly" });
     process.log.debug(" <- authController.logout");
   } catch (err) {
     process.log.error(` x- authController.logout ERROR: ${err.message}`);
