e71770e1318582d6433e7cb31594dae8133ad78c
Rafa <rafa.linares.molina@gmail.com>
2020-10-18 14:27:29 +0200

    files formatted

--- routers/dentistRouter.js
+++ routers/dentistRouter.js
@@ -2,8 +2,11 @@ const router = require("express").Router();
 const {
   createAppointment,
   cancelAppointment,
-  modifyAppointment,
+  acceptAppointment,
+  endAppointment,
+  modifyAccountData,
   deactivateAcount,
+  modifyAppointment,
   watchHistoryOfAppointments,
   watchHistoryOfAppointmentsFromPatient,
   watchHistoryOfAppointmentsBetweenDates,
@@ -11,15 +14,17 @@ const {
 const auth = require("../middleware/auth");
 
 router.post("/appointment", auth.loggedRequired, auth.dentistRequired, createAppointment);
-router.delete("/appointment", auth.loggedRequired, auth.dentistRequired, cancelAppointment);
+router.put("/appointment", auth.loggedRequired, auth.dentistRequired, createAppointment);
+router.put("/appointment/cancel", auth.loggedRequired, auth.dentistRequired, cancelAppointment);
+router.put("/appointment/confirm", auth.loggedRequired, auth.dentistRequired, acceptAppointment);
+router.put("/appointment/done", auth.loggedRequired, auth.dentistRequired, endAppointment);
+router.delete("/appointment", auth.loggedRequired, auth.dentistRequired, modifyAppointment);
+
+router.put("/account", auth.loggedRequired, auth.dentistRequired, modifyAccountData);
 router.delete("/account", auth.loggedRequired, auth.dentistRequired, deactivateAcount);
+
 router.get("/appointments", auth.loggedRequired, auth.dentistRequired, watchHistoryOfAppointments);
 router.get("/appointments/user", auth.loggedRequired, auth.dentistRequired, watchHistoryOfAppointmentsFromPatient);
-router.get(
-  "/appointmentsbetweenDates",
-  auth.loggedRequired,
-  auth.dentistRequired,
-  watchHistoryOfAppointmentsBetweenDates
-);
+router.get("/appointmentsbetweenDates", auth.loggedRequired, auth.dentistRequired, watchHistoryOfAppointmentsBetweenDates);
 
 module.exports = router;
