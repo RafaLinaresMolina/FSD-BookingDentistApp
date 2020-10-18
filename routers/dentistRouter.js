8a053bc492d621bd7ce9e5a8dea4e04c15fae814
Rafa <rafa.linares.molina@gmail.com>
2020-10-18 13:20:49 +0200

    added missing endpoint

--- routers/dentistRouter.js
+++ routers/dentistRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const {
   createAppointment,
   cancelAppointment,
+  modifyAppointment,
   deactivateAcount,
   watchHistoryOfAppointments,
   watchHistoryOfAppointmentsFromPatient,
