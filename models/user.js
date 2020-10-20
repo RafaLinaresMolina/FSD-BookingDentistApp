d30ade25c4202003b1e377b24d05472fdd214583
Rafa <rafa.linares.molina@gmail.com>
2020-10-20 18:26:32 +0200

    removed unused modes, also added methods on model User

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const tools = require("../lib/tools");
 
 const UserSchema = mongoose.Schema({
   token: {
@@ -41,6 +43,81 @@ const UserSchema = mongoose.Schema({
     required: true,
   },
 });
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  user.password = await bcrypt.hash(user.password, 9);
+  next();
+});
+
+UserSchema.statics.checkCredentials = async function (credentials) {
+  try {
+    const userDocument = await this.findOne({ email: credentials.email });
+    if (!userDocument) {
+      throw Error("User not found");
+    }
+
+    return userDocument;
+  } catch (err) {
+    throw err;
+  }
+};
+
+UserSchema.statics.getUserById = async function (id) {
+  try {
+    process.log.debug(" -> UserSchema.statics.userFound");
+    const userDoc = await UserModel.findOne({ _id: id, status: { $ne: 0 } });
+    if (!userDoc) {
+      process.log.warning(" <- UserSchema.statics.userFound");
+      throw new Error(`User with Id '${id}' not found`);
+    }
+    process.log.debug(" <- UserSchema.statics.userFound");
+    return userDoc;
+  } catch (err) {
+    process.log.error(` x- UserSchema.statics.userFound: ${err.message}`);
+    throw err;
+  }
+};
+
+UserSchema.statics.logout = async function (token) {
+  try {
+    const user = await this.findOne({ token: token });
+    if (!user) {
+      process.log.warning(` <- authController.logout: User already not logged.`);
+      throw new Error("User already not logged.");
+    }
+    user.token = null;
+    await user.save();
+  } catch (err) {
+    throw err;
+  }
+};
+
+UserSchema.statics.getAllLoggedUsers = async function () {
+  try {
+    process.log.debug(" -> UserSchema.statics.getAllLoggedUsers");
+    const userDoc = await UserModel.findOne({ token: { $ne: "" } });
+    if (!userDoc) {
+      process.log.warning(" <- UserSchema.statics.getAllLoggedUsers");
+      throw new Error(`User with Id '${id}' not found`);
+    }
+    process.log.debug(" <- UserSchema.statics.getAllLoggedUsers");
+    return userDoc;
+  } catch (err) {
+    process.log.error(
+      ` x- UserSchema.statics.getAllLoggedUsers: ${err.message}`
+    );
+    throw err;
+  }
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id }, "mimamamemimamucho", {
+    expiresIn: "2y",
+  });
+  return token;
+};
 
 const UserModel = mongoose.model("User", UserSchema, "Users");
 
