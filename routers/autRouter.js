b4369a92e4170cd47f10156b0070a08159542b70
Rafa <rafa.linares.molina@gmail.com>
2020-10-25 21:19:02 +0100

    fixed routers

--- routers/autRouter.js
+++ routers/autRouter.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const {register, login, logout, getUserData} = require('../controllers/authController');
-
+const auth = require("../middleware/auth")
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
-router.get("/user", getUserData)
+router.get("/user", auth.loggedRequired, getUserData)
 
 module.exports = router;
--- routers/clientRouter.js
+++ routers/clientRouter.js
@@ -10,7 +10,7 @@ const {
 const auth = require("../middleware/auth");
 
 router.post("/appointment", auth.loggedRequired, createAppointment);
-router.delete("/appointment", auth.loggedRequired, cancelAppointment);
+router.delete("/appointment/:_id", auth.loggedRequired, cancelAppointment);
 router.put("/account", auth.loggedRequired, modifyAccountData);
 router.delete("/account", auth.loggedRequired, deactivateAcount);
 router.get("/appointments", auth.loggedRequired, watchHistoryOfAppointments);
